93f4936a53adbd7193266e5b4c54c2896302a76f
giant-paw <giantpawbusiness@gmail.com>
2025-05-15 14:08:49 +0700

    Menambahkan event untuk fokus tap, pilih gambar dari galeri, buka kamera, hapus gambar, clear snackbar, dan permintaan izin

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -13,3 +13,22 @@ final class TakePicture extends CameraEvent {
   final void Function(File ImageFile) onPictureTaken;
   TakePicture(this.onPictureTaken);
 }
+
+final class TapToFocus extends CameraEvent {
+  final Offset position;
+  final Size previewSize;
+  TapToFocus(this.position, this.previewSize);
+}
+
+final class PickImageFromGallery extends CameraEvent {}
+
+final class OpenCameraAndCapture extends CameraEvent {
+  final BuildContext context;
+  OpenCameraAndCapture(this.context);
+}
+
+final class DeleteImage extends CameraEvent {}
+
+final class ClearSnackbar extends CameraEvent {}
+
+final class RequestPermission extends CameraEvent {}
